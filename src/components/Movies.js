8d5654d7d4e41540cf737b9ed1bf831fc3b757cc
Rolex Omondi <rolexomondi46@gmail.com>
2023-05-01 15:12:54 +0300

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const Movielist = movies.map((item) => (
+    <div key={item.title}>
+      <h2>{item.title}</h2>
+      <p>Runtime: {item.time} min.</p>
+      <ul>
+        {item.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return (
+   <div>
+  <h1>Movies Page</h1>
+  {Movielist}
+  </div>
+  );
 }
 
 export default Movies;
